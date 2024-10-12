d5c9fca02e21456831f19ede622f2de40af45254
Mateusz Drabik <md305966@student.polsl.pl>
2024-10-12 23:20:23 +0200

    commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
-///export LD_LIBRARY_PATH=...:$LD_LIBRARY_PATH
+
 [DllImport("../../../libc.so")]
 extern static int add(int a, int b);
 
@@ -8,6 +8,6 @@ extern static int add(int a, int b);
 extern static void hello();
 
 int a = 5;
-int b =2123;
+int b = 2123;
 Console.WriteLine("{0} + {1} = {2}", a, b, add(a,b));
 hello();
