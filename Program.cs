9035daeb3d8acce4498b3966ef1fbe7ed9554844
Mateusz Drabik <md305966@student.polsl.pl>
2024-10-17 13:01:44 +0200

    thread

--- Program.cs
+++ Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 
 [DllImport("../../../libc.so")]
 extern static int add(int a, int b);
 
-[DllImport("../../../libasm.so", EntryPoint ="_start")]
+[DllImport("../../../libasm.so", EntryPoint = "_start")]
 extern static void hello();
+
+[DllImport("../../../libasm.so", EntryPoint = "increment_array")]
+extern static int add1(ref int a, int size);
 
 int a = 5;
 int b = 2123;
-Console.WriteLine("{0} + {1} = {2}", a, b, add(a,b));
+Console.WriteLine("{0} + {1} = {2}", a, b, add(a, b));
 hello();
+int[] ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+const int numberOfThreads = 4;
+int chunkSize = ints.Length/numberOfThreads;
+
+Thread[] threads = new Thread[numberOfThreads];
+for (int i = 0; i < numberOfThreads; ++i)
+{
+    int t = i;
+    threads[i] = new Thread(() => add1(ref ints[t * chunkSize], 3));
+    threads[i].Start();
+}
+foreach (var i in ints)
+{
+    Console.WriteLine(i);
+}
--- asm/sim.asm
+++ asm/sim.asm
@@ -3,6 +3,7 @@ msg: db "Hello World", 0Ah
 len: equ $-msg
 segment .text
 	global _start
+	global increment_array
 
 _start:
 	mov rax, 1
@@ -11,3 +12,12 @@ _start:
 	mov rdx, len
 	syscall
     ret
+
+increment_array:
+Loop:	mov rax, [rdi + 4*rsi-4]
+		inc rax
+		mov [rdi + 4*rsi-4], rax
+		dec rsi
+		test rsi,rsi
+		jnz Loop
+		ret
